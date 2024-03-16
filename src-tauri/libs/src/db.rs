e1f03d049f681ce270395a8fb642079fe57298b9
JeseKi <2094901072@qq.com>
2024-03-16 10:25:42 +0800

    现在数据库文件将被创建在持久目录2

--- src-tauri/libs/src/db.rs
+++ src-tauri/libs/src/db.rs
@@ -1,7 +1,21 @@
-use rusqlite::{params, Connection, Result};
+use rusqlite::{Connection, params,Result};
+use std::error::Error;
+use dirs;
 
-pub fn connect() -> Result<Connection, rusqlite::Error> {
-    let conn = Connection::open("../pathlinker.db")?;
+pub fn connect() -> Result<Connection, Box<dyn Error>> {
+    // 定义数据库的路径，同时为了在Windows、Linux、MacOS上均可自动根据Home目录进行创建就使用dirs进行自动检测和创建
+    let mut db_path = dirs::data_local_dir().ok_or("Unable to find data local directory")?;
+    db_path.push("pathlinker");
+    db_path.push("pathlinker.db");
+
+    // 确保目录存在
+    if let Err(e) = std::fs::create_dir_all(db_path.parent().unwrap()) {
+        eprintln!("Failed to create directory: {}", e);
+        return Err(Box::new(e));
+    }
+
+    println!("Database path: {:?}", db_path);
+    let conn = Connection::open(db_path)?;
 
     // 创建一个新表，如果它还不存在的话
     conn.execute(
